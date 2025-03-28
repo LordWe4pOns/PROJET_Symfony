7eac8f79b10fb0858f6693dd5e8f8b6dfbe96902
LordWe4pOns <simon.cossais@orange.fr>
2025-03-28 12:21:01 +0100

    ajout gestions utilisateurs

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Form\EditUserType;
+use App\Entity\User;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -40,5 +40,39 @@ final class UserController extends AbstractController
             'editForm' => $form,
             'user' => $user,
         ]);
+    }
+
+    #[Route('/gestion/clients', name: '_gestion_clients')]
+    public function gestionClientsAction(EntityManagerInterface $entityManager): Response
+    {
+        $clients = $entityManager->getRepository(User::class)->findAll();
+
+        return $this->render('user/clients.html.twig', ['clients' => $clients]);
+    }
+
+    #[Route('/delete/{id}', name: '_delete', requirements: ['id' => '[1-9]\d*'])]
+    public function deleteAction(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $user = $entityManager->getRepository(User::class)->find($id);
+
+        if (is_null($user))
+            throw $this->createNotFoundException('erreur suppression client ' . $id . ' : le client n\'existe pas');
+
+        if ($this->getUser() !== $user && !in_array('ROLE_ADMIN', $user->getRoles()) && !in_array('ROLE_SUPER_ADMIN', $user->getRoles()))
+        {
+            $cart = $user->getCart();
+            if (!is_null($cart))
+            {
+                $content = $cart->getContent()->getValues();
+                for ($i = 0; $i < count($content); $i++)
+                {
+                    $content[$i]->setStock($content[$i]->getStock() + 1);
+                }
+                $entityManager->remove($cart);
+            }
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+        return $this->redirectToRoute('user_gestion_clients');
     }
 }
