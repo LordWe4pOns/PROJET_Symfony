f53237bdfdb57088dac55c605351914a3a204270
LordWe4pOns <simon.cossais@orange.fr>
2025-03-31 14:59:27 +0200

    correction panier

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -87,7 +87,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        $roles[] = 'ROLE_NOROLE';
 
         return array_unique($roles);
     }
