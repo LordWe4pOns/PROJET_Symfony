c04fb82df42d467aa9a477d5d1a9eecf017f5315
LordWe4pOns <simon.cossais@orange.fr>
2025-04-02 13:34:27 +0200

    mise a jour bd

--- migrations/Version20250402113309.php
+++ migrations/Version20250402113309.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250402105956 extends AbstractMigration
+final class Version20250402113309 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,15 +20,6 @@ final class Version20250402105956 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE l3_booster_country (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, booster_id INTEGER NOT NULL, country_id INTEGER NOT NULL, CONSTRAINT FK_1F2D40ECF85E4930 FOREIGN KEY (booster_id) REFERENCES l3_booster (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_1F2D40ECF92F3E70 FOREIGN KEY (country_id) REFERENCES l3_country (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
-        $this->addSql('CREATE INDEX IDX_1F2D40ECF85E4930 ON l3_booster_country (booster_id)');
-        $this->addSql('CREATE INDEX IDX_1F2D40ECF92F3E70 ON l3_booster_country (country_id)');
-        $this->addSql('CREATE TEMPORARY TABLE __temp__l3_booster AS SELECT id, expansion_id, name, price, stock FROM l3_booster');
-        $this->addSql('DROP TABLE l3_booster');
-        $this->addSql('CREATE TABLE l3_booster (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, expansion_id INTEGER NOT NULL, name VARCHAR(100) NOT NULL, price DOUBLE PRECISION NOT NULL, stock INTEGER NOT NULL, CONSTRAINT FK_C1F1D1265C15249D FOREIGN KEY (expansion_id) REFERENCES l3_expansion (id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT DEFERRABLE INITIALLY IMMEDIATE)');
-        $this->addSql('INSERT INTO l3_booster (id, expansion_id, name, price, stock) SELECT id, expansion_id, name, price, stock FROM __temp__l3_booster');
-        $this->addSql('DROP TABLE __temp__l3_booster');
-        $this->addSql('CREATE INDEX IDX_C1F1D1265C15249D ON l3_booster (expansion_id)');
         $this->addSql('CREATE TEMPORARY TABLE __temp__l3_user AS SELECT id, country_id, login, roles, password, name, surname, birthday FROM l3_user');
         $this->addSql('DROP TABLE l3_user');
         $this->addSql('CREATE TABLE l3_user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, country_id INTEGER DEFAULT NULL, login VARCHAR(180) NOT NULL, roles CLOB NOT NULL --(DC2Type:json)
@@ -42,14 +33,6 @@ final class Version20250402105956 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE l3_booster_country');
-        $this->addSql('CREATE TEMPORARY TABLE __temp__l3_booster AS SELECT id, expansion_id, name, price, stock FROM l3_booster');
-        $this->addSql('DROP TABLE l3_booster');
-        $this->addSql('CREATE TABLE l3_booster (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, expansion_id INTEGER NOT NULL, country_id INTEGER NOT NULL, name VARCHAR(100) NOT NULL, price DOUBLE PRECISION NOT NULL, stock INTEGER NOT NULL, CONSTRAINT FK_C1F1D1265C15249D FOREIGN KEY (expansion_id) REFERENCES l3_expansion (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_C1F1D126F92F3E70 FOREIGN KEY (country_id) REFERENCES l3_country (id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT DEFERRABLE INITIALLY IMMEDIATE)');
-        $this->addSql('INSERT INTO l3_booster (id, expansion_id, name, price, stock) SELECT id, expansion_id, name, price, stock FROM __temp__l3_booster');
-        $this->addSql('DROP TABLE __temp__l3_booster');
-        $this->addSql('CREATE INDEX IDX_C1F1D1265C15249D ON l3_booster (expansion_id)');
-        $this->addSql('CREATE INDEX IDX_C1F1D126F92F3E70 ON l3_booster (country_id)');
         $this->addSql('CREATE TEMPORARY TABLE __temp__l3_user AS SELECT id, country_id, login, roles, password, name, surname, birthday FROM l3_user');
         $this->addSql('DROP TABLE l3_user');
         $this->addSql('CREATE TABLE l3_user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, country_id INTEGER NOT NULL, login VARCHAR(180) NOT NULL, roles CLOB NOT NULL --(DC2Type:json)
